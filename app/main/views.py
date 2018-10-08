ea14a3f421b0811255336c809687b0788a2b3ef4
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-08 12:31:55 +0300

    new views.py

--- app/main/views.py
+++ app/main/views.py
@@ -6,10 +6,15 @@ from ..request import get_sources, get_articles
 
 @main.route('/')
 def index():
+    sources = get_sources()
+    print(sources)
     message = 'News Highlight'
-    return render_template('index.html',message = message)
+    return render_template('index.html',message=message, sources=sources)
 
-@main.route('/news/<int:news_id>')
-def news(news_id):
+@main.route('/news/<int:id>')
+def news(id):
+    articles = get_articles(id)
+    print(articles)
 
-    return render_template('news.html',  )
+
+    return render_template('news.html',news=articles, name='News Highlight')
