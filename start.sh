bcc23e4ec73981d6bd6995d3507cd6c96f51ce3c
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-08 18:15:44 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY=07ce08d46b364b099abea22526d7075c
-export SECRET_KEY=Joe
+export NEWS_API_KEY='07ce08d46b364b099abea22526d7075c'
+export SECRET_KEY='Joe'
 
 python3.6 manage.py server
