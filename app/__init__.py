962a9a74aaa5e321b8a0863135f2cd322365014d
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-08 09:25:40 +0300

    created more classes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from .config import DevConfig
 
 app = Flask(__name__)
 
-app.config.from_object(Devconfig)
+app.config.from_object(DevConfig)
 app.config.from_object('config.py')
 
 from app import views
--- app/models/news.py
+++ app/models/news.py
@@ -1,9 +1,14 @@
-class News:
-    def __init__(self,source,author,title,description,url,urltoImage,publishedAt,content):
+class Source:
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+
+class Articles:
+    def __init__(self,source,author,title,description,url,urltoImage,publishedAt):
         self.source = source
         self.author = author
         self.title = title
+        self.description = description
         self.url = url
         self.urltoImage = urltoImage
         self.publishedAt = publishedAt
-        self.content = content
